a5dd8249b69d39350c7a746259c7519b37a64c87
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-02-17 17:01:25 +0300

    Update deps; integrate with GitHub Actions.

--- gen/leaked/leaked_api.pb.go
+++ gen/leaked/leaked_api.pb.go
@@ -179,11 +179,11 @@ var fileDescriptor_cc6921d07712af2f = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // LeakedAPIClient is the client API for LeakedAPI service.
 //
@@ -195,10 +195,10 @@ type LeakedAPIClient interface {
 }
 
 type leakedAPIClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewLeakedAPIClient(cc *grpc.ClientConn) LeakedAPIClient {
+func NewLeakedAPIClient(cc grpc.ClientConnInterface) LeakedAPIClient {
 	return &leakedAPIClient{cc}
 }
 
