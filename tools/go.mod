2581cfbbe4b07a7824ff1c94b19b2c053f70cec5
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-02-24 21:20:32 +0300

    Move deps into separate package.

---
+++ tools/go.mod
@@ -0,0 +1,13 @@
+module github.com/percona-platform/platform/tools
+
+go 1.13
+
+// some dependecies should be synced with Dockerfile
+
+require (
+	github.com/golang/protobuf v1.3.3
+	github.com/grpc-ecosystem/grpc-gateway v1.13.0
+	github.com/mwitkow/go-proto-validators v0.3.0
+	github.com/uber/prototool v1.9.0
+	golang.org/x/tools v0.0.0-20200221224223-e1da425f72fd
+)
--- tools/tools.go
+++ tools/tools.go
@@ -1,13 +1,8 @@
 // +build tools
 
-package pp // import "github.com/percona-platform/platform"
+package tools // import "github.com/percona-platform/platform/tools"
 
 import (
-	_ "github.com/gogo/protobuf/protoc-gen-gofast"
-	_ "github.com/gogo/protobuf/protoc-gen-gogo"
-	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/gogo/protobuf/protoc-gen-gogofaster"
-	_ "github.com/gogo/protobuf/protoc-gen-gogoslick"
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
