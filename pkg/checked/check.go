8412ca53cc5b7661e11a1ce7ef1b97a67943fddc
Artem Gavrilov <artem.gavrilov@percona.com>
2020-04-08 21:49:06 +0200

    SAAS-43 Add check result, documentation and tests

--- pkg/checked/check.go
+++ pkg/checked/check.go
@@ -7,14 +7,11 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type Check struct {
-	Type   string `yaml:"type"`
-	Query  string `yaml:"query"`
-	Script string `yaml:"script"`
-}
-
-func Parse(r io.Reader) ([]*Check, error) {
-	d := yaml.NewDecoder(r)
+// ParseChecks returns slice of checks parsed form YAML passed via reader.
+// Can handle multi-document YAMLs, in that case output will be
+// union of checks presented in each file.
+func ParseChecks(reader io.Reader) ([]*Check, error) {
+	d := yaml.NewDecoder(reader)
 
 	type checks struct {
 		Checks []*Check `yaml:"checks"`
@@ -37,6 +34,33 @@ func Parse(r io.Reader) ([]*Check, error) {
 	return res, nil
 }
 
+// ParseCheck returns single check parsed from YAML passed as byte slice.
+func ParseCheck(b []byte) (*Check, error) {
+	var c Check
+	if err := yaml.Unmarshal(b, &c); err != nil {
+		return nil, errors.Wrap(err, "failed to parse check")
+	}
+
+	return &c, nil
+}
+
+// Supported check types
+const (
+	MySQLShow           = "MYSQL_SHOW"
+	MySQLSelect         = "MYSQL_SELECT"
+	PostgreSQLShow      = "POSTGRESQL_SHOW"
+	PostgreSQLSelect    = "POSTGRESQL_SELECT"
+	MongoDBGetParameter = "MONGODB_GETPARAMETER"
+)
+
+// Check represents security check structure
+type Check struct {
+	Type   string `yaml:"type"`
+	Query  string `yaml:"query"`
+	Script string `yaml:"script"`
+}
+
+// Validate validates check for minimal correctness.
 func (c *Check) Validate() error {
 	if err := c.validateType(); err != nil {
 		return err
@@ -53,19 +77,100 @@ func (c *Check) Validate() error {
 	return nil
 }
 
+// validateType validates check type
 func (c *Check) validateType() error {
 	switch c.Type {
-	case "MYSQL_SHOW":
+	case MySQLShow:
 		fallthrough
-	case "MYSQL_SELECT":
+	case MySQLSelect:
 		fallthrough
-	case "POSTGRESQL_SHOW":
+	case PostgreSQLShow:
 		fallthrough
-	case "POSTGRESQL_SELECT":
+	case PostgreSQLSelect:
+		fallthrough
+	case MongoDBGetParameter:
 		return nil
 	case "":
 		return errors.New("check type is empty")
 	default:
 		return errors.Errorf("unknown check type: %s", c.Type)
 	}
+}
+
+// Possible result statuses
+const (
+	Success = "SUCCESS"
+	Fail    = "FAIL"
+)
+
+// Parse returns slice of results parsed form YAML passed via reader.
+// Can handle multi-document YAMLs, in that case output will be
+// union of results presented in each file.
+func ParseResults(reader io.Reader) ([]*Result, error) {
+	d := yaml.NewDecoder(reader)
+
+	type results struct {
+		Results []*Result `yaml:"results"`
+	}
+
+	res := make([]*Result, 0)
+	for {
+		var r results
+		err := d.Decode(&r)
+		if err != nil {
+			if err != io.EOF {
+				return nil, errors.Wrap(err, "failed to parse results")
+			}
+			break
+		}
+
+		res = append(res, r.Results...)
+	}
+
+	return res, nil
+}
+
+// ParseResult returns single Result parsed from YAML passed as byte slice.
+func ParseResult(b []byte) (*Result, error) {
+	var r Result
+	if err := yaml.Unmarshal(b, &r); err != nil {
+		return nil, errors.Wrap(err, "failed to parse result")
+	}
+
+	return &r, nil
+}
+
+// Result represents check result that has status and message.
+// In case of FAIL status, message should contain reason.
+type Result struct {
+	Status  string `yaml:"status"`
+	Message string `yaml:"message"`
+}
+
+// Validate validates check result for minimal correctness.
+func (r *Result) Validate() error {
+	if err := r.validateStatus(); err != nil {
+		return err
+	}
+
+	if r.Status == Fail && r.Message == "" {
+		return errors.New("failed check result should have message")
+	}
+
+	return nil
+}
+
+// validateType validates check result status
+func (r *Result) validateStatus() error {
+	switch r.Status {
+	case Success:
+		fallthrough
+	case Fail:
+		return nil
+	case "":
+		return errors.New("result status is empty")
+	default:
+		return errors.Errorf("unknown result status: %s", r.Status)
+	}
+
 }
--- pkg/checked/check_test.go
+++ pkg/checked/check_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var singleFileData = `---
+func TestCheck_ParseChecks(t *testing.T) {
+	t.Run("singleFile", func(t *testing.T) {
+		var data = `---
 checks:
   - type: MYSQL_SHOW
     query: VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');
@@ -17,7 +19,17 @@ checks:
             pass
 `
 
-var multipleFilesData = `---
+		cs, err := ParseChecks(bytes.NewReader([]byte(data)))
+		require.NoError(t, err)
+
+		assert.Len(t, cs, 1)
+		assert.Equal(t, "MYSQL_SHOW", cs[0].Type)
+		assert.Equal(t, "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", cs[0].Query)
+		assert.Equal(t, cs[0].Script, "def function1(args):\n    pass\n")
+	})
+
+	t.Run("multipleFiles", func(t *testing.T) {
+		var data = `---
 checks:
   - type: MYSQL_SHOW
     query: VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');
@@ -32,22 +44,7 @@ checks:
         def function2(args):
             pass
 `
-
-func TestCheck_Parse(t *testing.T) {
-
-	t.Run("singleFile", func(t *testing.T) {
-		cs, err := Parse(bytes.NewReader([]byte(singleFileData)))
-		require.NoError(t, err)
-
-		assert.Len(t, cs, 1)
-		assert.Equal(t, "MYSQL_SHOW", cs[0].Type)
-		assert.Equal(t, "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", cs[0].Query)
-		assert.Equal(t, cs[0].Script, "def function1(args):\n    pass\n")
-	})
-
-
-	t.Run("multipleFiles", func(t *testing.T) {
-		cs, err := Parse(bytes.NewReader([]byte(multipleFilesData)))
+		cs, err := ParseChecks(bytes.NewReader([]byte(data)))
 		require.NoError(t, err)
 
 		assert.Len(t, cs, 2)
@@ -60,26 +57,176 @@ func TestCheck_Parse(t *testing.T) {
 		assert.Equal(t, "id, name FROM table WHERE id=123;", cs[1].Query)
 		assert.Equal(t, cs[1].Script, "def function2(args):\n    pass\n")
 	})
-
 }
 
-func TestCheck_validateType(t *testing.T) {
+func TestCheck_ParseCheck(t *testing.T) {
+	data := `type: MYSQL_SHOW
+query: VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');
+script: |
+    def function1(args):
+        pass
+`
+
+	c, err := ParseCheck([]byte(data))
+	require.NoError(t, err)
+
+	assert.Equal(t, "MYSQL_SHOW", c.Type)
+	assert.Equal(t, "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", c.Query)
+	assert.Equal(t, c.Script, "def function1(args):\n    pass\n")
+}
+
+func TestCheck_CheckValidate(t *testing.T) {
 	tests := []struct {
 		name   string
-		typ    string
+		check  *Check
 		errStr string
 	}{
-		{name: "mysql_show", typ: "MYSQL_SHOW", errStr: ""},
-		{name: "mysql_select", typ: "MYSQL_SELECT", errStr: ""},
-		{name: "postgresql_show", typ: "POSTGRESQL_SHOW", errStr: ""},
-		{name: "postgresql_select", typ: "POSTGRESQL_SHOW", errStr: ""},
-		{name: "clickhouse_show", typ: "CLICKHOUSE_SHOW", errStr: "unknown check type: CLICKHOUSE_SHOW"},
-		{name: "empty", typ: "", errStr: "check type is empty"},
+		{
+			name:   "mysql_show",
+			check:  &Check{Type: "MYSQL_SHOW", Query: "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", Script: "def func(args): pass"},
+			errStr: "",
+		},
+		{
+			name:   "mysql_select",
+			check:  &Check{Type: "MYSQL_SELECT", Query: "id, name FROM table WHERE id=123;", Script: "def func(args): pass"},
+			errStr: "",
+		},
+		{
+			name:   "postgresql_show",
+			check:  &Check{Type: "POSTGRESQL_SHOW", Query: "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", Script: "def func(args): pass"},
+			errStr: "",
+		},
+		{
+			name:   "postgresql_select",
+			check:  &Check{Type: "POSTGRESQL_SELECT", Query: "id, name FROM table WHERE id=123;", Script: "def func(args): pass"},
+			errStr: "",
+		},
+		{
+			name:   "mongodb_get_parameter",
+			check:  &Check{Type: "MONGODB_GETPARAMETER", Query: "\"saslHostName\" : 1", Script: "def func(args): pass"},
+			errStr: "",
+		},
+		{
+			name:   "clickhouse_show",
+			check:  &Check{Type: "CLICKHOUSE_SHOW", Query: "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", Script: "def func(args): pass"},
+			errStr: "unknown check type: CLICKHOUSE_SHOW",
+		},
+		{
+			name:   "empty_type",
+			check:  &Check{Type: "", Query: "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", Script: "def func(args): pass"},
+			errStr: "check type is empty",
+		},
+		{
+			name:   "empty_query",
+			check:  &Check{Type: "MYSQL_SHOW", Query: "", Script: "def func(args): pass"},
+			errStr: "check query is empty",
+		},
+		{
+			name:   "empty_script",
+			check:  &Check{Type: "MYSQL_SHOW", Query: "VARIABLES WHERE Variable_name IN ('have_ssl', 'have_openssl');", Script: ""},
+			errStr: "check script is empty",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := &Check{Type: tt.typ}
-			err := c.validateType()
+			err := tt.check.Validate()
+
+			if tt.errStr != "" {
+				assert.EqualError(t, err, tt.errStr)
+				return
+			}
+
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestCheck_ParseResults(t *testing.T) {
+	t.Run("singleFile", func(t *testing.T) {
+		var data = `---
+results:
+  - status: FAIL
+    message: something bad happened
+`
+		cs, err := ParseResults(bytes.NewReader([]byte(data)))
+		require.NoError(t, err)
+
+		assert.Len(t, cs, 1)
+		assert.Equal(t, "FAIL", cs[0].Status)
+		assert.Equal(t, "something bad happened", cs[0].Message)
+	})
+
+	t.Run("multipleFiles", func(t *testing.T) {
+		var data = `---
+results:
+  - status: FAIL
+    message: something bad happened
+---
+results:
+  - status: SUCCESS
+    message: 
+`
+		cs, err := ParseResults(bytes.NewReader([]byte(data)))
+		require.NoError(t, err)
+
+		assert.Len(t, cs, 2)
+
+		assert.Equal(t, "FAIL", cs[0].Status)
+		assert.Equal(t, "something bad happened", cs[0].Message)
+
+		assert.Equal(t, "SUCCESS", cs[1].Status)
+		assert.Empty(t,  cs[1].Message)
+	})
+}
+
+func TestCheck_ParseResult(t *testing.T) {
+	data := `status: FAIL
+message: something bad happened
+`
+
+	r, err := ParseResult([]byte(data))
+	require.NoError(t, err)
+
+	assert.Equal(t, "FAIL", r.Status)
+	assert.Equal(t, "something bad happened", r.Message)
+}
+
+func TestCheck_ResultValidate(t *testing.T) {
+	tests := []struct {
+		name   string
+		result *Result
+		errStr string
+	}{
+		{
+			name:   "success_result_without_message",
+			result: &Result{Status: Success, Message: ""},
+			errStr: "",
+		},
+		{
+			name:   "success_result_with_message",
+			result: &Result{Status: Success, Message: "everything is fine!"},
+			errStr: "",
+		},
+		{
+			name:   "failed_result_with_message",
+			result: &Result{Status: Fail, Message: "something bad happened!"},
+			errStr: "",
+		},
+		{
+			name:   "failed_result_without_message",
+			result: &Result{Status: Fail, Message: ""},
+			errStr: "failed check result should have message",
+		},
+		{
+			name:   "empty_status",
+			result: &Result{Status: "", Message: ""},
+			errStr: "result status is empty",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.result.Validate()
 
 			if tt.errStr != "" {
 				assert.EqualError(t, err, tt.errStr)
