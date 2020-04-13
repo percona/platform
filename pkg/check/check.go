a2a06408dec0c6d860aaaa4f217c6e684a8bc123
Artem Gavrilov <artem.gavrilov@percona.com>
2020-04-13 22:12:20 +0200

    SAAS-58 Add func for checks signatures verifing

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -2,11 +2,60 @@
 package check
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/base64"
 	"io"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 )
+
+func Verify(b []byte, key, sig string) (bool, error) {
+	kBin, err := base64.StdEncoding.DecodeString(key)
+	if err != nil || len(kBin) != 42 {
+		return false, errors.New("Invalid encoded public key")
+	}
+
+	lines := strings.SplitN(sig, "\n", 4)
+	if len(lines) < 4 {
+		return false, errors.New("Incomplete encoded signature")
+	}
+
+	sig1, err := base64.StdEncoding.DecodeString(lines[1])
+	if err != nil || len(sig1) != 74 {
+		return false, errors.New("Invalid encoded signature")
+	}
+	sigTComment := lines[2]
+	sig2, err := base64.StdEncoding.DecodeString(lines[3])
+	if err != nil || len(sig2) != 64 {
+		return false, errors.New("Invalid encoded signature")
+	}
+
+	sAlg, sKeyID, sSig := sig1[0:2], sig1[2:10], sig1[10:74]
+	kAlg, kKeyID, kKey := kBin[0:2], kBin[2:10], kBin[10:42]
+
+	if bytes.Equal(kAlg, sAlg) {
+		return false, errors.New("Incompatible signature algorithm")
+	}
+	if sAlg[0] != 0x45 || sAlg[1] != 0x64 {
+		return false, errors.New("Unsupported signature algorithm")
+	}
+	if bytes.Equal(kKeyID, sKeyID) {
+		return false, errors.New("Incompatible key identifiers")
+	}
+	if !strings.HasPrefix(sigTComment, "trusted comment: ") {
+		return false, errors.New("Unexpected format for the trusted comment")
+	}
+	if !ed25519.Verify(ed25519.PublicKey(kKey), b, sSig) {
+		return false, errors.New("Invalid signature")
+	}
+	if !ed25519.Verify(ed25519.PublicKey(kKey), append(sSig[:], []byte(sigTComment)[17:]...), sig2) {
+		return false, errors.New("Invalid global signature")
+	}
+	return true, nil
+}
 
 // Parse returns slice of checks parsed from YAML passed via reader.
 // Can handle multi-document YAMLs, parsing result will be a single slice
