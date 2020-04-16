914f01e2b220842ea5b5b6b286c5eb66f33a7710
Jiří Čtvrtka <jiri.ctvrtka@percona.com>
2020-04-16 17:00:03 +0200

    SAAS-57 Remove unused func for now

--- pkg/starlark/converts.go
+++ pkg/starlark/converts.go
@@ -21,22 +21,3 @@ func goToStarlark(v interface{}) starlark.Value {
 		panic(fmt.Sprintf("goToStarlark: unhandled %#[1]v (%[1]T)", v))
 	}
 }
-
-func starlarkToGo(v starlark.Value) interface{} {
-	switch v := v.(type) {
-	case starlark.Int:
-		if i, ok := v.Int64(); ok {
-			return int(i)
-		}
-		if u, ok := v.Uint64(); ok {
-			return uint(u)
-		}
-		panic(fmt.Sprintf("starlarkToGo: unhandled starlark.Int %s", v.String()))
-	case starlark.Float:
-		return v
-	case starlark.String:
-		return string(v)
-	default:
-		panic(fmt.Sprintf("starlarkToGo: unhandled %#[1]v (%[1]T)", v))
-	}
-}
