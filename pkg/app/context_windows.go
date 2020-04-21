77aee3361ebf0316bb6a99c1141655af56640c78
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-04-21 14:38:51 +0300

    Fix build on Windows.

--- pkg/app/context_windows.go
+++ pkg/app/context_windows.go
@@ -1,14 +1,11 @@
-// +build !windows
+// +build windows
 
 package app
 
 import (
 	"context"
-	"os"
-	"os/signal"
 
 	"go.uber.org/zap"
-	"golang.org/x/sys/unix"
 
 	"github.com/percona-platform/platform/pkg/logger"
 )
@@ -20,14 +17,16 @@ func Context() context.Context {
 	ctx, cancel := context.WithCancel(context.Background())
 	ctx = logger.GetCtxWithLogger(ctx, l)
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, unix.SIGTERM, unix.SIGINT)
-	go func() {
-		s := <-signals
-		signal.Stop(signals)
-		l.Sugar().Warnf("Got %s, shutting down...", unix.SignalName(s.(unix.Signal)))
-		cancel()
-	}()
+	_ = cancel
+	// TODO
+	// signals := make(chan os.Signal, 1)
+	// signal.Notify(signals, unix.SIGTERM, unix.SIGINT)
+	// go func() {
+	// 	s := <-signals
+	// 	signal.Stop(signals)
+	// 	l.Sugar().Warnf("Got %s, shutting down...", unix.SignalName(s.(unix.Signal)))
+	// 	cancel()
+	// }()
 
 	return ctx
 }
