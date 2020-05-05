d04cff87c16b24b19dcb468e3c990572bfaabc2f
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-05-05 13:10:21 +0300

    Add dummy file

---
+++ platform.go
@@ -0,0 +1,6 @@
+// Package platform contains private API definitions and common code.
+package platform
+
+// This file exist to make platform a valid Go package, so
+//   go get -v github.com/percona-platform/platform@latest
+// will work.
