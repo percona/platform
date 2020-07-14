6bbabb490456654054eea6c47f2807ea5d760ad3
Artem Gavrilov <artem.gavrilov@percona.com>
2020-07-14 19:56:05 +0200

    Allow to switch logger debug/production logger with env variable

--- pkg/logger/global.go
+++ pkg/logger/global.go
@@ -1,12 +1,24 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 )
 
+var debug = os.Getenv("PLATFORM_DEBUG") == "1"
+
 // SetupGlobal setups global zap logger.
 func SetupGlobal() {
-	l, err := zap.NewDevelopment()
+	var err error
+	var l *zap.Logger
+
+	if debug {
+		l, err = zap.NewDevelopment()
+	} else {
+		l, err = zap.NewProduction()
+	}
+
 	if err != nil {
 		panic(err)
 	}
