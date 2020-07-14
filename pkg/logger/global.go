1f606048a22e454e0dc4cf7a6041f2520bc15e44
Artem Gavrilov <artem.gavrilov@percona.com>
2020-07-14 19:59:20 +0200

    Refactoring

--- pkg/logger/global.go
+++ pkg/logger/global.go
@@ -6,14 +6,12 @@ import (
 	"go.uber.org/zap"
 )
 
-var debug = os.Getenv("PLATFORM_DEBUG") == "1"
-
 // SetupGlobal setups global zap logger.
 func SetupGlobal() {
 	var err error
 	var l *zap.Logger
 
-	if debug {
+	if os.Getenv("PLATFORM_DEBUG") == "1" {
 		l, err = zap.NewDevelopment()
 	} else {
 		l, err = zap.NewProduction()
