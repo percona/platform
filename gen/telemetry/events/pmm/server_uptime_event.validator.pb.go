721b36664723fa84633f2c213c6854649527e75b
Artem Gavrilov <artem.gavrilov@percona.com>
2020-07-28 19:54:15 +0200

    Update tools, regenerate code

--- gen/telemetry/events/pmm/server_uptime_event.validator.pb.go
+++ gen/telemetry/events/pmm/server_uptime_event.validator.pb.go
@@ -6,19 +6,16 @@ package pmmv1
 import (
 	fmt "fmt"
 	math "math"
-
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/duration"
 	_ "github.com/mwitkow/go-proto-validators"
+	_ "github.com/golang/protobuf/ptypes/duration"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
-var (
-	_ = proto.Marshal
-	_ = fmt.Errorf
-	_ = math.Inf
-)
+var _ = proto.Marshal
+var _ = fmt.Errorf
+var _ = math.Inf
 
 func (this *ServerUptimeEvent) Validate() error {
 	if !(len(this.Id) == 16) {
