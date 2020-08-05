141afc0ad7283474651b47342281e0c881737db9
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-08-05 15:58:13 +0300

    SAAS-253 Tweak logging (#102)
    * Allow to switch logger debug/production logger with env variable
    
    * Refactoring
    
    * SAAS-253 Tweak logging config
    
    * SAAS-253 Use struct
    
    * SAAS-253 Handle nil values
    
    * SAAS-253 Fix typo
    
    Co-authored-by: Artem Gavrilov <artem.gavrilov@percona.com>

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,17 +2,23 @@
 package app
 
 import (
+	"os"
 	"runtime/debug"
+	"strconv"
 
 	"github.com/pkg/errors"
 	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/percona-platform/platform/pkg/logger"
 )
 
 // Config is basic Percona Platform application configuration.
 type Config struct {
-	GRPCAddr  string
-	HTTPAddr  string
-	DebugAddr string
+	GRPCAddr  string // gRPC Server address
+	HTTPAddr  string // HTTP Server address
+	DebugAddr string // debug Server address
+
+	LoggerOpts logger.SetupGlobalOpts
 }
 
 func version() string {
@@ -79,6 +85,17 @@ func Setup(opts *SetupOpts) (*Config, error) {
 	}
 
 	kingpin.Flag("debug.addr", "Debug listen address").Default(":20203").StringVar(&config.DebugAddr)
+
+	// use global environment variables PLATFORM_LOG_XXX for defaults values,
+	// but allow to set flags via normal APP_PLATFORM_LOG_XXX environment variables
+	b, _ := strconv.ParseBool(os.Getenv("PLATFORM_LOG_DEBUG"))
+	logDebugDefault := strconv.FormatBool(b)
+	kingpin.Flag("log.debug", "Enable debug level logging").
+		Default(logDebugDefault).BoolVar(&config.LoggerOpts.LogDebug)
+	b, _ = strconv.ParseBool(os.Getenv("PLATFORM_LOG_DEVMODE"))
+	logDevMode := strconv.FormatBool(b)
+	kingpin.Flag("log.devmode", "Enable development mode loging: text instead of JSON, DPanic panics instead of logging errors").
+		Default(logDevMode).BoolVar(&config.LoggerOpts.LogDevMode)
 
 	return &config, nil
 }
--- pkg/logger/global.go
+++ pkg/logger/global.go
@@ -4,9 +4,36 @@ import (
 	"go.uber.org/zap"
 )
 
+// SetupGlobalOpts contains logger options.
+type SetupGlobalOpts struct {
+	LogDebug   bool // enable debug level logging
+	LogDevMode bool // enable development mode logging: text instead of JSON, DPanic panics instead of logging errors
+}
+
 // SetupGlobal setups global zap logger.
-func SetupGlobal() {
-	l, err := zap.NewDevelopment()
+func SetupGlobal(opts *SetupGlobalOpts) {
+	if opts == nil {
+		opts = new(SetupGlobalOpts)
+	}
+
+	cfg := &zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:      false,
+		Encoding:         "json",
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	if opts.LogDebug {
+		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+	if opts.LogDevMode {
+		cfg.Development = true
+		cfg.Encoding = "console"
+		cfg.EncoderConfig = zap.NewDevelopmentEncoderConfig()
+	}
+
+	l, err := cfg.Build()
 	if err != nil {
 		panic(err)
 	}
