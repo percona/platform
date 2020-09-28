ff517b5a524678bd50622176ab4e33a505c0cbb1
Artem Gavrilov <artem.gavrilov@percona.com>
2020-09-28 21:37:27 +0200

    SAAS-16 Resolve linter warnings

--- pkg/telemetry/telemetry_test.go
+++ pkg/telemetry/telemetry_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 
 	pmmv1 "github.com/percona-platform/platform/gen/telemetry/events/pmm"
 	reporterv1 "github.com/percona-platform/platform/gen/telemetry/reporter"
@@ -24,7 +24,7 @@ func TestValidators(t *testing.T) {
 	}
 	err := event.Validate()
 	require.NoError(t, err)
-	assert.NotContains(t, proto.MessageName(event), "beta")
+	assert.NotContains(t, event.ProtoReflect().Descriptor().FullName(), "beta")
 
 	eventB, err := proto.Marshal(event)
 	require.NoError(t, err)
@@ -35,12 +35,12 @@ func TestValidators(t *testing.T) {
 			Id:   id[:],
 			Time: ptypes.TimestampNow(),
 			Event: &reporterv1.AnyEvent{
-				TypeUrl: proto.MessageName(event),
+				TypeUrl: string(event.ProtoReflect().Descriptor().FullName()),
 				Binary:  eventB,
 			},
 		}},
 	}
 	err = req.Validate()
 	require.NoError(t, err)
-	assert.NotContains(t, proto.MessageName(req), "beta")
+	assert.NotContains(t, req.ProtoReflect().Descriptor().FullName(), "beta")
 }
