9bd95bb70196c850dc62c717187f30ce43febcbf
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-12-28 17:25:38 +0300

    PMM-7228 Clarify what is severity and add tests

--- pkg/common/severity_test.go
+++ pkg/common/severity_test.go
@@ -7,31 +7,36 @@ import (
 )
 
 func TestParseSeverity(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
-		name     string
-		str      string
-		severity Severity
+		input     string
+		severity  Severity
+		canonical string
 	}{
-		{name: "normal", str: "Emergency", severity: Emergency},
-		{name: "first lowercase ", str: "alert", severity: Alert},
-		{name: "first space", str: " critical", severity: Critical},
-		{name: "last tabs", str: "Error		", severity: Error},
-		{name: "all capital", str: "WARNING", severity: Warning},
-		{name: "normal", str: "notice", severity: Notice},
-		{name: "normal", str: "Info", severity: Info},
-		{name: "normal", str: "Debug", severity: Debug},
-		{name: "normal", str: "Unknown", severity: Unknown},
-		{name: "empty string", str: "", severity: Unknown},
-		{name: "spaces", str: "     ", severity: Unknown},
-		{name: "unknown", str: "awesome", severity: Unknown},
+		{input: "Emergency", severity: Emergency, canonical: "emergency"},
+		{input: "alert", severity: Alert, canonical: "alert"},
+		{input: " critical", severity: Critical, canonical: "critical"},
+		{input: "Error		", severity: Error, canonical: "error"},
+		{input: "WARNING", severity: Warning, canonical: "warning"},
+		{input: "notice", severity: Notice, canonical: "notice"},
+		{input: "Info", severity: Info, canonical: "info"},
+		{input: "Debug", severity: Debug, canonical: "debug"},
+		{input: "Unknown", severity: Unknown, canonical: "unknown"},
+		{input: "", severity: Unknown, canonical: "unknown"},
+		{input: "     ", severity: Unknown, canonical: "unknown"},
+		{input: "awesome", severity: Unknown, canonical: "unknown"},
 	}
 
 	for _, tt := range tests {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			actual := ParseSeverity(tt.str)
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+
+			actual := ParseSeverity(tt.input)
 
 			assert.Equal(t, tt.severity, actual)
+			assert.Equal(t, tt.canonical, actual.String())
 		})
 	}
 }
