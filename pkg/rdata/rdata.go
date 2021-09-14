d1579035bb116f4e73d44321deb70d9de97026ca
Maxim Kondratenko <maxim.kondratenko@percona.com>
2021-09-14 14:15:58 +0300

    SAAS-628 Add gRPC auth info propagation (#268)
    * SAAS-628 Add gRPC auth info propagation
    
    * SAAS-628 Fix linter issues
    
    * SAAS-628 Add more rebust error exaplnation for unauthenticated requests.

--- pkg/rdata/rdata.go
+++ pkg/rdata/rdata.go
@@ -10,14 +10,35 @@ import (
 type requestDataKey struct{}
 
 // RequestData contains request related data added by interceptors.
+// See description: https://confluence.percona.com/display/PMM/Single+Sign-On+-+Portal+Integration+with+Okta#SingleSignOnPortalIntegrationwithOkta-AuthorizationInfoHeaders
 type RequestData struct {
+	// Username Percona Account username that is used for authentication.
+	Username string
+
+	// UserID Percona Account User ID in Okta.
+	// Note: Percona Account is handled by Okta so ID comes from Okta as well.
+	UserID string
+
+	// IsPortalSuperAdmin flag indicates that this particular user has SuperAdmin
+	// permissions in Percona Portal only.
+	IsPortalSuperAdmin bool
+
+	// PortalOrgID Percona Portal Organization ID (equal to Okta Group ID).
+	PortalOrgID string
+
+	// AuthToken holds OAuth2 access_token that was used for request authentication.
+	// Is used for token propagation to outgoing requests since 'Authorization'
+	// HTTP header is removed by Traefik after request authentication.
+	AuthToken string
+
+	// Keep for backward compatibility
 	UserEmail string
 	SessionID string
 }
 
 // AddToContext adds session id and user email to request context.
-func AddToContext(ctx context.Context, sessionID, userEmail string) context.Context {
-	return context.WithValue(ctx, requestDataKey{}, &RequestData{SessionID: sessionID, UserEmail: userEmail})
+func AddToContext(ctx context.Context, data *RequestData) context.Context {
+	return context.WithValue(ctx, requestDataKey{}, data)
 }
 
 // GetFromContext extracts request data from request context.
