e1b2ec247093d45bdb82dc46184c0df3b730ae00
Maxim Kondratenko <max.kondr@gmail.com>
2021-11-26 17:44:56 +0200

    SAAS-692 Add SearchOrganizationTickets API (#283)

--- gen/org/org_api.validator.pb.go
+++ gen/org/org_api.validator.pb.go
@@ -244,3 +244,27 @@ func (this *UpdateMemberResponse) Validate() error {
 	}
 	return nil
 }
+func (this *SearchOrganizationTicketsRequest) Validate() error {
+	if this.OrgId == "" {
+		return github_com_mwitkow_go_proto_validators.FieldError("OrgId", fmt.Errorf(`value '%v' must not be an empty string`, this.OrgId))
+	}
+	return nil
+}
+func (this *SearchOrganizationTicketsResponse) Validate() error {
+	for _, item := range this.Tickets {
+		if item != nil {
+			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
+				return github_com_mwitkow_go_proto_validators.FieldError("Tickets", err)
+			}
+		}
+	}
+	return nil
+}
+func (this *OrganizationTicket) Validate() error {
+	if this.CreateTime != nil {
+		if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(this.CreateTime); err != nil {
+			return github_com_mwitkow_go_proto_validators.FieldError("CreateTime", err)
+		}
+	}
+	return nil
+}
