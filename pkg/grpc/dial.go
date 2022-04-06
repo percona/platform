2c7aebca408235c89d63d9117fa89aad4a206f34
Artem Gavrilov <artem.gavrilov@percona.com>
2022-04-06 17:55:10 +0200

    PMM-9700 Add dial fucntion for gRPC connection creation

---
+++ pkg/grpc/dial.go
@@ -0,0 +1,24 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Dial creates gRPC connection for a given address.
+func Dial(ctx context.Context, address, userAgent string) (grpc.ClientConnInterface, error) {
+	opts := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithUserAgent(userAgent),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	conn, err := grpc.DialContext(ctx, address, opts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to dial %s", address)
+	}
+
+	return conn, nil
+}
