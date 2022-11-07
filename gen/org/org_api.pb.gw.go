047c09e5dc3b3a1f0e43d27048ce05a572afb895
Dmytro Chukmasov <35612129+dchukmasov@users.noreply.github.com>
2022-11-07 12:29:23 +0200

    SAAS-1000: automated user onboarding (#557)
    * SAAS-1000: orgd OnboardMember definitions
    
    * SAAS-1000: adding okta find group by name method
    
    * SAAS-1000: add option to provide first name and last name during user registration
    
    * SAAS-1000: lint fix and unit test
    
    * SAAS-1000: renaming in definitions
    
    * SAAS-1000: adding platform_org_id to OnboardMemberResponse
    
    * update golang.org/x/text-v0.3.7 dependency
    
    * go mod tidy
    
    * SAAS-1000: removing grpc validation
    
    * SAAS-1000: allow post body to onboard method

--- gen/org/org_api.pb.gw.go
+++ gen/org/org_api.pb.gw.go
@@ -829,6 +829,40 @@ func local_request_OrgAPI_SearchUserCompany_0(ctx context.Context, marshaler run
 
 }
 
+func request_OrgAPI_OnboardMember_0(ctx context.Context, marshaler runtime.Marshaler, client OrgAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq OnboardMemberRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.OnboardMember(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_OrgAPI_OnboardMember_0(ctx context.Context, marshaler runtime.Marshaler, server OrgAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq OnboardMemberRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.OnboardMember(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_OrgAPI_ConnectPMM_0(ctx context.Context, marshaler runtime.Marshaler, client OrgAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ConnectPMMRequest
 	var metadata runtime.ServerMetadata
@@ -1445,6 +1479,31 @@ func RegisterOrgAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 
 	})
 
+	mux.Handle("POST", pattern_OrgAPI_OnboardMember_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/percona.platform.org.v1.OrgAPI/OnboardMember", runtime.WithHTTPPathPattern("/v1/orgs/members:onboard"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_OrgAPI_OnboardMember_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_OrgAPI_OnboardMember_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_OrgAPI_ConnectPMM_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1969,6 +2028,28 @@ func RegisterOrgAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("POST", pattern_OrgAPI_OnboardMember_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/percona.platform.org.v1.OrgAPI/OnboardMember", runtime.WithHTTPPathPattern("/v1/orgs/members:onboard"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_OrgAPI_OnboardMember_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_OrgAPI_OnboardMember_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_OrgAPI_ConnectPMM_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2155,6 +2236,8 @@ var (
 
 	pattern_OrgAPI_SearchUserCompany_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "orgs", "company"}, "search"))
 
+	pattern_OrgAPI_OnboardMember_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "orgs", "members"}, "onboard"))
+
 	pattern_OrgAPI_ConnectPMM_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "orgs", "inventory"}, ""))
 
 	pattern_OrgAPI_ConnectPMM_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "orgs", "inventory"}, "connect"))
@@ -2198,6 +2281,8 @@ var (
 	forward_OrgAPI_SearchOrganizationTickets_0 = runtime.ForwardResponseMessage
 
 	forward_OrgAPI_SearchUserCompany_0 = runtime.ForwardResponseMessage
+
+	forward_OrgAPI_OnboardMember_0 = runtime.ForwardResponseMessage
 
 	forward_OrgAPI_ConnectPMM_0 = runtime.ForwardResponseMessage
 
