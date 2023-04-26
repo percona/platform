0274044b6385f15a0ac96789adf2dcbef2a45651
Oksana Grishchenko <oksana.grishchenko@percona.com>
2023-04-26 11:33:27 +0200

    generated files

--- gen/org/org_api.pb.gw.go
+++ gen/org/org_api.pb.gw.go
@@ -1685,7 +1685,7 @@ func RegisterOrgAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 // RegisterOrgAPIHandlerFromEndpoint is same as RegisterOrgAPIHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterOrgAPIHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
